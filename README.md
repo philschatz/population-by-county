7fddc4e6b415cff0546083ab3566930781647652
Philip Schatz <253202+philschatz@users.noreply.github.com>
2020-03-29 19:20:39 -0500

    add population JSON

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # population-by-county
+
+The file [fips-population.json](./fips-population.json) contains a set of key/value pairs where the key is the FIPS code for a county and the value is the population. ([Raw CSV File here](https://raw.githubusercontent.com/philschatz/population-by-county/master/fips-population.csv))
---
+++ build.js
@@ -0,0 +1,44 @@
+(async () => {
+  const {writeFileSync} = require('fs');
+  const pify = require('pify');
+  const citysdk_ = require("citysdk");
+  const citysdk = pify(citysdk_);
+
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+  function pad(num) {
+    if (num < 10) {
+      return `0${num}`
+    } else {
+      return `${num}`
+    }
+  }
+
+  let acc = {};
+
+  for (let state = 0; state < 60; state++) {
+    console.log(`Fetching state ${state}`);
+    const res = await citysdk({
+      vintage : 2017,        // required
+      geoHierarchy : {       // required  
+        state: pad(state), // https://www.census.gov/programs-surveys/geography/guidance/geo-identifiers.html
+        // state : {
+        //   lat : 45.5103513,
+        //   lng : -122.6740118
+        // },
+        county: '*'
+      },
+      // Source: https://github.com/loganpowell/census-geojson/tree/master/public/census#parameters
+      sourcePath : ["pep","population"],  // required 
+      values : ["DENSITY","POP"]     // required 
+    });
+
+    res.forEach(item => {
+      acc[`${item.state}${item.county}`] = item.POP
+    })
+    await sleep(500);
+  }
+
+  writeFileSync('fips-population.json', JSON.stringify(acc, null, 2));
+
+})();
+
